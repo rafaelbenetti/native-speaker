9450f8a4dfd195fabc20f71967a4dd63fe334aff
Rafael de Oliveira Benetti <rafaeldeoliveirabenetti@gmail.com>
2017-04-21 19:47:53 -0300

    refactor(user): add some validation on angular js

--- src/public/js/controllers/account-controller.js
+++ src/public/js/controllers/account-controller.js
@@ -6,14 +6,57 @@
         .controller('AccountController', ['$location', 'accountService', AccountController]);
 
     const HOME_URL = location.origin;
+    const countries = [{
+            name: 'English US',
+            code: 'US'
+        },
+        {
+            name: 'English UK',
+            code: 'GB'
+        },
+        {
+            name: 'Spanish',
+            code: 'ES'
+        },
+        {
+            name: 'French',
+            code: 'FR'
+        },
+        {
+            name: 'German',
+            code: 'DE'
+        },
+        {
+            name: 'Irish',
+            code: 'IR'
+        },
+        {
+            name: 'Italian',
+            code: 'IT'
+        },
+        {
+            name: 'Japonese',
+            code: 'JP'
+        },
+        {
+            name: 'Korean',
+            code: 'KS'
+        },
+        {
+            name: 'Portuguese',
+            code: 'BR'
+        }
+    ];
 
     function AccountController($location, accountService) {
         let controller = this;
+        controller.countries = countries;
         controller.account = {
             languages: []
         };
 
         controller.create = function (account) {
+            console.log(account);
             accountService.create(account)
                 .then((result) => $location.path('/'),
                     (result) => {
@@ -31,9 +74,26 @@
                     });
         };
 
-        controller.addLanguage = () => controller.account.languages.push({});
+        controller.addLanguage = () => {
+            controller.account.languages.push({});
+            controller.updateCountries();
+        };        
         controller.removeLanguage = (index) => {
             controller.account.languages.splice(index, 1);
+            controller.updateCountries();
+        };
+        controller.updateCountries = () => {
+            controller.countries.map((c) =>
+                c.used = controller.account.languages.filter((l) => l.name === c.code)[0]);
+        };
+
+        controller.selectLanguage = (code) => {
+            let country = countries.filter((c) => c.code === code)[0];
+            let index = countries.indexOf(country);
+
+            country.used = true;
+            controller.countries[index] = country;
+            controller.updateCountries();
         };
     };
 })();
